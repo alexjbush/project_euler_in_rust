f216c21a4b0a4f9b74bb35e53e47109688cef094
Alex Bush <bushnoh@gmail.com>
2021-08-09 21:39:04 +1200

    src/problem_002.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
+mod problem_002;
+
 fn main() {
     println!("Project Euler!");
 
-    println!("Choose a challenge to run:");
+    loop {
+        let mut choice = String::new();
 
-    let mut choice = String::new();
+        println!("Choose a challenge to run:");
+        println!("2: Even Fibonacci numbers");
+        println!("q: Quit");
 
-    std::io::stdin()
-    .read_line(&mut choice)
-    .expect("Failed to read option");
+        std::io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read option");
 
-    println!("You chose: {}", choice);
-
+        match choice.trim() {
+            "q" => break,
+            "2" => {
+                problem_002::run();
+                break;
+            }
+            _ => println!("Unknown option: {}", choice),
+        }
+    }
 }
